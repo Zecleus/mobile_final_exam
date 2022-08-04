fddee3ba3ee0e9bed05d271503d3d9d79ddfd963
Kenneth Cuico <kenneth.cuico123@gmail.com>
2022-08-04 13:42:02 +0800

    initial commit with task bloc already created

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:bloc_finals_exam/blocs/bloc_exports.dart';
+import 'package:flutter/material.dart';
+
+import 'app_router.dart';
+import 'app_themes.dart';
+import 'screens/tabs_screen.dart';
+
+void main() {
+  BlocOverrides.runZoned(() => runApp(
+        MyApp(appRouter: AppRouter()),
+      ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key, required this.appRouter}) : super(key: key);
+
+  final AppRouter appRouter;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => TasksBloc(),
+      child: MaterialApp(
+        title: 'BloC Tasks App',
+        theme: AppThemes.appThemeData[AppTheme.lightMode],
+        home: const TabsScreen(),
+        onGenerateRoute: appRouter.onGenerateRoute,
+      ),
+    );
+  }
+}
